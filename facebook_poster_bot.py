20032ae6fca5993cfe44fa2e5132631a925e5e5c
Rahim Iqbal <raheemabdur203@gmail.com>
2026-07-25 07:47:21 +0000

    fixe import issue

--- facebook_poster_bot.py
+++ facebook_poster_bot.py
@@ -7,11 +7,13 @@ from datetime import datetime
 from selenium import webdriver
 from selenium.common.exceptions import (
     TimeoutException,
+    StaleElementReferenceException,
     ElementClickInterceptedException,
 )
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 from webdriver_manager.chrome import ChromeDriverManager
